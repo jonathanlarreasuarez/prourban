37295b09dfa86b7ad83729b79dc26f2dd013764c
justhineT <jusatumb@gmail.com>
2017-07-30 00:44:15 -0500

    crud de proveedores

--- app/controllers/administrador/proveedorController.js
+++ app/controllers/administrador/proveedorController.js
@@ -4,13 +4,17 @@ angular.module('ProUrban')
 .controller('proveedorController', ['$scope', '$rootScope', '$location', 'localStorageService', 'ProveedorService',
 	function($scope, $rootScope, $location, localStorageService, ProveedorService) {
 		$scope.getProveedores = getProveedores;
+		$scope.insertarProveedor = insertarProveedor;
+		$scope.modificarProveedor = modificarProveedor;
+		$scope.eliminarProveedor = eliminarProveedor;
+		$scope.buscarProveedor = buscarProveedor;
 
 		function getProveedores() {
 			ProveedorService.getProveedores()
 			.then(function(response) {
 				// MANEJO DE RESPUESTA
 				response = JSON.parse(response.respuesta)
-				
+
 				if (response.codigo === 1) {
 					$scope.data = response.datos;
 				} else {
@@ -22,5 +26,82 @@ angular.module('ProUrban')
 		}
 
 		$scope.getProveedores();
+
+		//insertar Proveedor
+		function insertarProveedor() {
+			// console.log('descripcion:::', ($scope.descripcion);
+			// console.log('ruc:::', $scope.ruc);
+			ProveedorService.insertarProveedor($scope.descripcion, $scope.ruc)
+			.then(function(response) {
+				// MANEJO DE RESPUESTA
+				response = JSON.parse(response.respuesta)
+
+				if (response.codigo === 1) {
+					$scope.data = response.datos;
+				} else {
+					alert(response.mensaje);
+				}
+			}, function(err){
+				// MANEJO DE ERRORES
+			});
+		}
+		//modificar Proveedor
+		function modificarProveedor() {
+			// console.log('descripcion:::', ($scope.descripcion);
+			// console.log('ruc:::', $scope.ruc);
+			ProveedorService.modificarProveedor($scope.modDescripcion, $scope.modRuc, $scope.modId)
+			.then(function(response) {
+				// MANEJO DE RESPUESTA
+				response = JSON.parse(response.respuesta)
+
+				if (response.codigo === 1) {
+					$scope.data = response.datos;
+				} else {
+					alert(response.mensaje);
+				}
+			}, function(err){
+				// MANEJO DE ERRORES
+			});
+		}
+
+		//eliminar Proveedor
+		function eliminarProveedor() {
+			console.log('descripcion:::', $scope.delId);
+			// console.log('ruc:::', $scope.ruc);
+			ProveedorService.eliminarProveedor($scope.delId)
+			.then(function(response) {
+				// MANEJO DE RESPUESTA
+				response = JSON.parse(response.respuesta)
+
+				if (response.codigo === 1) {
+					$scope.data = response.datos;
+				} else {
+					alert(response.mensaje);
+				}
+			}, function(err){
+				// MANEJO DE ERRORES
+			});
+		}
+
+		//buscar el Proveedor para luego ser modificado
+		function buscarProveedor() {
+			console.log('descripcion:::', $scope.modId);
+			// console.log('ruc:::', $scope.ruc);
+			ProveedorService.buscarProveedor($scope.modId)
+			.then(function(response) {
+				// MANEJO DE RESPUESTA
+				response = JSON.parse(response.respuesta)
+
+				if (response.codigo === 1) {
+					$scope.modi = response.datos;
+					console.log($scope.modi);
+				} else {
+					alert(response.mensaje);
+				}
+			}, function(err){
+				// MANEJO DE ERRORES
+			});
+		}
+
 	}
 ]);
--- app/factories/administrador/proveedorService.js
+++ app/factories/administrador/proveedorService.js
@@ -10,12 +10,44 @@ angular.module('ProUrban')
 
 		service.getProveedores = getProveedores;
 
+		service.insertarProveedor = insertarProveedor;
+
+		service.modificarProveedor = modificarProveedor;
+
+		service.eliminarProveedor = eliminarProveedor;
+
+		service.buscarProveedor = buscarProveedor;
+
 		return service;
 
 		function getProveedores() {
 			//	Realiza la llamada al servicio web enviando los parámetros
 			//	en formato JSON
 			return $soap.post(base_url,"ListaProveedores");
+		}
+
+		function insertarProveedor(descripcion, ruc) {
+			//	Realiza la llamada al servicio web enviando los parámetros
+			//	en formato JSON
+			return $soap.post(base_url,"insertarProveedor", {descripcion: descripcion, ruc: ruc});
+		}
+
+		function buscarProveedor(id) {
+			//	Realiza la llamada al servicio web enviando los parámetros
+			//	en formato JSON
+			return $soap.post(base_url,"buscarProveedor", {id: id});
+		}
+
+		function modificarProveedor(descripcion, ruc, id) {
+			//	Realiza la llamada al servicio web enviando los parámetros
+			//	en formato JSON
+			return $soap.post(base_url,"modificarProveedor", {descripcion: descripcion, ruc: ruc, id: id});
+		}
+
+		function eliminarProveedor(id) {
+			//	Realiza la llamada al servicio web enviando los parámetros
+			//	en formato JSON
+			return $soap.post(base_url,"eliminarProveedor", {id: id});
 		}
 
 }]);
