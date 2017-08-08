f1985249317cc483c5091d0d4bd60049b7821fb3
Ronny Valdivieso <ron_251@hotmail.com>
2017-08-07 19:00:05 -0500

    Revert "resolver conflictos del merge"
    This reverts commit a8a8b25c6e724387c597b4a111fc195391e84065.

--- app/factories/administrador/proveedorService.js
+++ app/factories/administrador/proveedorService.js
@@ -10,6 +10,13 @@ angular.module('ProUrban')
 		service.eliminarProveedor = eliminarProveedor;
 		service.buscarProveedor = buscarProveedor;
 
+		service.insertarProveedor = insertarProveedor;
+
+		service.modificarProveedor = modificarProveedor;
+
+		service.eliminarProveedor = eliminarProveedor;
+
+		service.buscarProveedor = buscarProveedor;
 
 		return service;
 
@@ -45,6 +52,30 @@ angular.module('ProUrban')
 			//	en formato JSON
 			return $soap.post(AppConfig.apiUrl, "EliminarProveedor",
 				{ id: id });
+		}
+
+		function insertarProveedor(descripcion, ruc) {
+			//	Realiza la llamada al servicio web enviando los parámetros
+			//	en formato JSON
+			return $soap.post(base_url,"insertarProveedor", {descripcion: descripcion, ruc: ruc});
+		}
+
+		function buscarProveedor(id) {
+			//	Realiza la llamada al servicio web enviando los parámetros
+			//	en formato JSON
+			return $soap.post(base_url,"buscarProveedor", {id: id});
+		}
+
+		function modificarProveedor(descripcion, ruc, id) {
+			//	Realiza la llamada al servicio web enviando los parámetros
+			//	en formato JSON
+			return $soap.post(base_url,"modificarProveedor", {descripcion: descripcion, ruc: ruc, id: id});
+		}
+
+		function eliminarProveedor(id) {
+			//	Realiza la llamada al servicio web enviando los parámetros
+			//	en formato JSON
+			return $soap.post(base_url,"eliminarProveedor", {id: id});
 		}
 
 }]);
